0cc0e111427fbcc2b4ff5d817c2d9666cd1398de
ArkhamKnightG <kenanismayilzade12@.com>
2020-11-28 02:52:33 +0300

    Task2_1

---
+++ task2_1.js
@@ -0,0 +1,27 @@
+const str1 = "radar";
+function isPalindrome(str) {
+    for (let i = str.length - 1; i >= 0; i--) {
+        reversedString = reversedString + str[I];
+
+    }
+    if (reversedString === str) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+// gives result as "false" because reversedString is always = "firstLetter"
+
+
+let rvsword = isPalindrome();
+if (rvsword === isPalindrome("reversedString")) {
+    return result;
+
+} else {
+    return false;
+}
+
+const firstWord = reallyPalindrome(str1);
+console.log(firstWord);
+//always gives error "Cannot read property 'length' of undefined"
